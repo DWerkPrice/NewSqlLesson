903443b14539e9250b45537eba0f314994ce48d4
David Price <dwerkprice@gmail.com>
2020-02-20 16:29:03 -0500

    First Commit

--- NewSqlLesson/Program.cs
+++ NewSqlLesson/Program.cs
@@ -11,15 +11,42 @@ namespace SqlLesson
 
             var sqllib = new BcConnection();
             sqllib.Connect(@"localhost\sqlexpress" , "EdDb" , "trusted_connection=true");
+            MajorController.bcConnection = sqllib;
+
+            var majors = MajorController.GetAllMajors();
+            foreach(var major in majors) {
+                Console.WriteLine(major);
+            }
+
+
             StudentController.bcConnection = sqllib; // setting property to instance
 
-            var student = StudentController.GetStudentByPK(100);
+            var newStudent = new Student {
+            Id = 885,
+            Firstname = "Crazy",
+            Lastname = "Eights",
+            SAT = 600,
+            GPA = 0.00,
+            MajorId = 1
+            };
+          //  var success = StudentController.InsertStudent(newStudent);
+
+            var student = StudentController.GetStudentByPK(newStudent.Id);
             if(student == null) {
                 Console.WriteLine("Student with primary key not found");
 
             }else {
                 Console.WriteLine(student);
             }
+            student.Firstname = "Charlie";
+            student.Lastname = "Chan";
+            var success = StudentController.UpdateStudent(student);
+/*
+            var studentToDelete = new Student {
+                Id = 885
+            };
+            success = StudentController.DeleteStudent(885);
+*/
 
             var students = StudentController.GetAllStudents();
             foreach(var student0 in students) {
--- SqlLibrary/StudentController.cs
+++ SqlLibrary/StudentController.cs
@@ -5,19 +5,20 @@ using System.Text;
 
 namespace SqlLibrary
 {
-    public class StudentController
-    {
+    public class StudentController {
         // establish connection
-   
+
         public static BcConnection bcConnection { get; set; }
 
-       
+
         public static List<Student> GetAllStudents() { // pass the database an sql command
-            var sql = "SELECT * From Student";// sql syntax command to be passed
+            var sql = "SELECT * From Student s left join Major m on m.Id= s.MajorId;";// sql syntax command to be passed
             var command = new SqlCommand(sql , bcConnection.Connection);
-        // reads the database and if statement checks for actual row of result
+            // reads the database and if statement checks for actual row of result
             var reader = command.ExecuteReader();
             if (!reader.HasRows) {
+                reader.Close();
+                reader = null;
                 Console.WriteLine("No rows from GetAllStudents()");
                 return new List<Student>();
             }
@@ -34,7 +35,19 @@ namespace SqlLibrary
                 student.Lastname = reader["Lastname"].ToString();
                 student.SAT = Convert.ToInt32(reader["SAT"]);
                 student.GPA = Convert.ToDouble(reader["GPA"]);
-                //student.MajorId = Convert.ToInt32(reader["MajorId"]);
+                if (Convert.IsDBNull(reader["Description"])) {
+
+                    student.Major = null;
+
+
+                } else {
+                    var major = new Major {
+                        Description = reader["Description"].ToString() ,
+                        MinSAT = Convert.ToInt32(reader["MinSat"])
+
+                    };
+                }
+                //         student.MajorId = Convert.IsDBNull(reader["MajorId"])) ? null: Conve;
                 students.Add(student);  // adds student to collection of students
             }// checks for row beyond bottom
             reader.Close();
@@ -47,6 +60,8 @@ namespace SqlLibrary
             var command = new SqlCommand(sql , bcConnection.Connection); // gets command ready to go
             var reader = command.ExecuteReader();
             if (!reader.HasRows) {
+                reader.Close();
+                reader = null;
                 return null;
             }
             reader.Read();
@@ -59,8 +74,85 @@ namespace SqlLibrary
             //student.MajorId = Convert.ToInt32(reader["MajorId"]);
             reader.Close();
             reader = null;
-            return student;              
+            return student;
 
+        }
+
+        public static bool InsertStudent(Student student) {
+            var majorid = "";
+            if (student.MajorId == null) {
+                majorid = "NULL";
+            } else {
+                majorid = student.MajorId.ToString();
+            }
+
+
+            //var sql = $"INSERT into Student (Id, Firstname, Lastname, SAT, GPA, MajorId) " +
+            //$"VALUES ({student.Id} , '{student.Firstname}', '{student.Lastname}', {student.SAT}, {student.GPA}, {majorid};";
+            //  @region SQL with parameters
+            var sql = $"INSERT into Student (Id, Firstname, Lastname, SAT, GPA, MajorId) "
+            + $"Values(@Id, @Firstname, @Lastname, @SAT, @GPA, @MajorId); ";
+
+
+            var command = new SqlCommand(sql , bcConnection.Connection);
+            command.Parameters.AddWithValue("@Id" , student.Id);
+            command.Parameters.AddWithValue("@Firstname" , student.Firstname);
+            command.Parameters.AddWithValue("@Lastname" , student.Lastname);
+            command.Parameters.AddWithValue("@SAT" , student.SAT);
+            command.Parameters.AddWithValue("@GPA" , student.GPA);
+            command.Parameters.AddWithValue("@MajorId" , student.MajorId ?? Convert.DBNull);
+            //recs plural because it will be used in other methods that can affect more than one 
+            var recsAffected = command.ExecuteNonQuery();
+            if (recsAffected != 1) {
+                Console.WriteLine("Here");
+                throw new Exception("Insert Failed"); // most likely failure is in the data passed
+                // no return needed because throw exits the method on its own
+            }
+            return true;
+        }
+
+        public static bool UpdateStudent(Student student) {
+            var sql = "Update Student Set" +
+                " Firstname = @Firstname', " +
+                " Lastname = @Lastname, " +
+                " SAT= @SAT" +
+                " GPA = @GPA" +
+                " MajorId = @MajorID " +
+                " Where Id = @Id; ";
+            var command = new SqlCommand(sql , bcConnection.Connection);
+            command.Parameters.AddWithValue("@Id" , student.Id);
+            command.Parameters.AddWithValue("@Firstname" , student.Firstname);
+            command.Parameters.AddWithValue("@Lastname" , student.Lastname);
+            command.Parameters.AddWithValue("@SAT" , student.SAT);
+            command.Parameters.AddWithValue("@GPA" , student.GPA);
+            command.Parameters.AddWithValue("@MajorId" , student.MajorId ?? Convert.DBNull);
+            //recs plural because it will be used in other methods that can affect more than one 
+            var recsAffected = command.ExecuteNonQuery();
+            if (recsAffected != 1) {
+                throw new Exception("Update Failed"); // most likely failure is in the data passed
+                // no return needed because throw exits the method on its own
+            }
+            return true;
+        }
+
+        public static bool DeleteStudent(Student student) {
+            var sql = "delete from student where id = @Id";
+            var command = new SqlCommand(sql , bcConnection.Connection);
+            command.Parameters.AddWithValue("@Id" , student.Id);
+            var recsAffected = command.ExecuteNonQuery();
+            if (recsAffected != 1) {
+                throw new Exception("Delete Failed");
+            }
+            return true;
+        }
+
+        public static bool DeleteStudent(int id) {
+            var student = GetStudentByPK(id);
+            if (student == null) {
+                return false;
+            }
+            var success = DeleteStudent(student);
+            return true;
         }
     }
 }
